6d772c5f1f80495392c71cd22a5d99e5e24e2685
SVV-2002 <90024570+SVV-2002@users.noreply.github.com>
2021-11-27 20:16:12 +0530

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,31 +1,33 @@
+//Person Object Creation
+function Person(first,last,address) {
+    this.FirstName=first;
+    this.LastName=last;
+    this["Office Address"]=address;
+}
+
 //Person1
-var person= {
-    //Person Object Creation
-    firstName: "Vaishnavi",
-    lastName: "Vinnakota",
-    officeAddress: "Visakhapatnam",
-};
+var person1=new Person("Vaishnavi","Vinnakota","Visakhapatnam");
 console.log("Person 1 Properties:");
-console.log("FirstName: "+person.firstName);
-console.log("LastName: "+person.lastName);
-console.log("OfficeAddress: "+person.officeAddress);
+console.log(person1);
+console.log("FirstName: "+person1.FirstName);
+console.log("LastName: "+person1.LastName);
+console.log("OfficeAddress: "+person1["Office Address"]);
 console.log("");
 
 //Person2
-person.firstName= "Sneha";
-person.lastName= "Garuda";
-person.officeAddress= "Hyderabad";
+var person2=new Person("Sneha","Garuda","Hyderabad");
 console.log("Person 2 Properties:");
-console.log("FirstName: "+person.firstName);
-console.log("LastName: "+person.lastName);
-console.log("OfficeAddress: "+person.officeAddress);
+console.log(person2);
+console.log("FirstName: "+person2.FirstName);
+console.log("LastName: "+person2.LastName);
+console.log("OfficeAddress: "+person2["Office Address"]);
 console.log("");
 
 //Person3
-person.firstName= "Sravanthi";
-person.lastName= "Garuda";
-person.officeAddress= "Kakinada";
-console.log("Person 3 Properties:");
-console.log("FirstName: "+person.firstName);
-console.log("LastName: "+person.lastName);
-console.log("OfficeAddress: "+person.officeAddress);
+var person3=new Person("Sravanthi","Garuda","Kakinada");
+console.log("Person 1 Properties:");
+console.log(person3);
+console.log("FirstName: "+person3.FirstName);
+console.log("LastName: "+person3.LastName);
+console.log("OfficeAddress: "+person3["Office Address"]);
+console.log("");
