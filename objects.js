f050df45e1ea788a38488ae33d3e974c747cf713
SVV-2002 <90024570+SVV-2002@users.noreply.github.com>
2021-11-27 18:16:17 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,31 @@
+//Person1
+var person= {
+    //Person Object Creation
+    firstName: "Vaishnavi",
+    lastName: "Vinnakota",
+    officeAddress: "Visakhapatnam",
+};
+console.log("Person 1 Properties:");
+console.log("FirstName: "+person.firstName);
+console.log("LastName: "+person.lastName);
+console.log("OfficeAddress: "+person.officeAddress);
+console.log("");
+
+//Person2
+person.firstName= "Sneha";
+person.lastName= "Garuda";
+person.officeAddress= "Hyderabad";
+console.log("Person 2 Properties:");
+console.log("FirstName: "+person.firstName);
+console.log("LastName: "+person.lastName);
+console.log("OfficeAddress: "+person.officeAddress);
+console.log("");
+
+//Person3
+person.firstName= "Sravanthi";
+person.lastName= "Garuda";
+person.officeAddress= "Kakinada";
+console.log("Person 3 Properties:");
+console.log("FirstName: "+person.firstName);
+console.log("LastName: "+person.lastName);
+console.log("OfficeAddress: "+person.officeAddress);
